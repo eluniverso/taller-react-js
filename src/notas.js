01d983d891b0ae519faafbdfe97934d6b96a4793
Ramon Chancay <ramon.chancay@gmail.com>
2018-10-14 16:46:58 -0500

    quitando de mas

--- src/notas.js
+++ src/notas.js
@@ -1,13 +1,5 @@
 const notas = [
   {
-    id: 1,
-    titulo: "Parroquia El Morro impresiona con sus aves y delfines",
-    slug: "/larevista/2018/10/14/nota/6995132/morro-tiene-alas-aletas-anhelos",
-    category: "noticias",
-    enlace: "https://via.placeholder.com/80x50",
-    descripcion: `Los guayaquileños desconocemos las maravillas naturales que tenemos a poca distancia. O, si las conocemos,`
-  },
-  {
     id: 2,
     titulo:
       "El nivel de hambruna es preocupante y alarmante en unos 60 países, según informe",
@@ -32,10 +24,13 @@ const notas = [
   },
   {
     id: 4,
-    titulo: "El nivel de hambruna es preocupante y alarmante en unos 60 países, según informe",
-    slug: "/noticias/2018/10/14/nota/7000487/nivel-hambruna-es-preocupante-alarmante-60-paises-segun-informe",
+    titulo:
+      "El nivel de hambruna es preocupante y alarmante en unos 60 países, según informe",
+    slug:
+      "/noticias/2018/10/14/nota/7000487/nivel-hambruna-es-preocupante-alarmante-60-paises-segun-informe",
     category: "noticias",
-    enlace: "https://www.eluniverso.com/sites/default/files/styles/fotogaleria_portada_secundario/public/fotos/2017/03/8390994.jpg?itok=S-OMl4Sf",
+    enlace:
+      "https://www.eluniverso.com/sites/default/files/styles/fotogaleria_portada_secundario/public/fotos/2017/03/8390994.jpg?itok=S-OMl4Sf",
     descripcion: `El nivel del hambre continúa siendo preocupante en una sesentena de países, según un índice mundial realizado por las oenegés irlandesa Concern Worldwide y la alemana Welthungerhilfe.`
   }
 ];
