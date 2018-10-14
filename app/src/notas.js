9bd6ea48d7d4892552a7115a10ff9cc4259f643e
nellaparedes <nella.paredes@gmail.com>
2018-10-14 16:15:27 -0500

    agrego segunda noticia

--- app/src/notas.js
+++ app/src/notas.js
@@ -9,11 +9,11 @@ const notas = [
   },
   {
     id: 2,
-    titulo: "Parroquia El Morro impresiona con sus aves y delfines",
-    slug: "/larevista/2018/10/14/nota/6995132/morro-tiene-alas-aletas-anhelos",
-    category: "noticias",
-    enlace: "https://via.placeholder.com/80x50",
-    descripcion: `Los guayaquileños desconocemos las maravillas naturales que tenemos a poca distancia. O, si las conocemos,`
+    titulo: "El nivel de hambruna es preocupante y alarmante en unos 60 países, según informe",
+    slug: "/noticias/2018/10/14/nota/7000487/nivel-hambruna-es-preocupante-alarmante-60-paises-segun-informe",
+    category: "internacional",
+    enlace: "https://www.eluniverso.com/sites/default/files/styles/nota_ampliada_normal_foto/public/fotos/2018/10/somaliahambruna1.jpg",
+    descripcion: "El nivel del hambre continúa siendo preocupante en una sesentena de países, según un índice mundial."
   }
 ];
 
